a9acab0a0a646024095f062d00a80404404efee0
Alexius DIAKOGIANNIS <alexius@jee.gr>
2016-08-09 11:34:58 +0300

    correction in licence

--- src/gr/jcrete/diakogiannis_alexius/shormavenprojectwindow/OpenShortProjects.java
+++ src/gr/jcrete/diakogiannis_alexius/shormavenprojectwindow/OpenShortProjects.java
@@ -1,5 +1,5 @@
 /**
-    *This file is part of Foobar.
+    *This file is part of NB-Short-Maven-Project-Window.
     *
     *Foobar is free software: you can redistribute it and/or modify
     *it under the terms of the GNU General Public License as published by
